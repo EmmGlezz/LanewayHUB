84e0dddd0804e68e6ed431105f77ad7a4c2ac942
EmmGlezz <contact@emilianogonzalez.com>
2023-03-16 10:52:42 +1100

    Redux Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,20 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/HomePage/HomePage";
+import LoginPage from "./scenes/LoginPage/LoginPage";
+import ProfilePage from "./scenes/ProfilePage/ProfilePage";
 
-function App() {
-  return (
-    <div className="app">
-      
-    </div>
-  );
-}
+const App = () => {
+	return (
+		<div className='app'>
+			<BrowserRouter>
+				<Routes>
+					<Route path='/' element={<LoginPage />} />
+					<Route path='/home' element={<HomePage />} />
+					<Route path='/profile/:userId' element={<ProfilePage />} />
+				</Routes>
+			</BrowserRouter>
+		</div>
+	);
+};
 
 export default App;
