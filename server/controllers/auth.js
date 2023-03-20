53bc74ae8d86ec4312af911badff1549699419ca
EmmGlezz <contact@emilianogonzalez.com>
2023-03-20 18:19:09 +1100

    Register debug on deployment test 5

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -35,7 +35,7 @@ exports.register = async (req, res) => {
 		const savedUser = await newUser.save();
 		res.status(201).json(savedUser);
 	} catch (error) {
-		res.status(500).json({ error: error.message });
+		res.status(500).json({ error: 'You reached register' });
 	}
 };
 
