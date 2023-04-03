01593d95d077e2fb7c996a842430d8701d658405
EmmGlezz <contact@emilianogonzalez.com>
2023-04-03 16:30:37 +1000

    Social media logo tweak 1

--- client/src/scenes/widgets/UserWidget.jsx
+++ client/src/scenes/widgets/UserWidget.jsx
@@ -103,7 +103,7 @@ const UserWidget = ({ userId, picturePath }) => {
 				{/* TWITTER */}
 				<FlexBetween gap='1rem' mb='0.5rem'>
 					<FlexBetween gap='1rem'>
-						<img src='https://laneway-hub-server.onrender.com/uploads/twitter.png' alt='twitter' />
+						<img src='public/assets/twitter.png' alt='twitter' />
 						<Box>
 							<Typography color={main} fontWeight='500'>
 								Twitter
@@ -117,7 +117,7 @@ const UserWidget = ({ userId, picturePath }) => {
 				{/* LINKEDIN */}
 				<FlexBetween gap='1rem'>
 					<FlexBetween gap='1rem'>
-						<img src='https://laneway-hub-server.onrender.com/uploads/linkedin.png' alt='linkedin' />
+						<img src='/linkedin.png' alt='linkedin' />
 						<Box>
 							<Typography color={main} fontWeight='500'>
 								LinkedIn
